b9bbc3e9c6353e0369fe801b154f554e46731694
Utheja0609 <uthejamaheshwaram@gmail.com>
2026-08-04 14:56:07 +0530

    second  commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mahes\OneDrive\Desktop\Clinovo-Assessment-System\venv
+set VIRTUAL_ENV=D:\Clinovo-Assessment-System\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
